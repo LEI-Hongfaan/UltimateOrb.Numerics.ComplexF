10949cc7ed1751a80dfc47a74f9139b706cb73d7
LEI-Hongfaan <lihangfan@hotmail.com>
2024-01-13 04:41:50 +0000

    Add project files.

---
+++ UltimateOrb.Numerics.ComplexF/UltimateOrb/Numerics/SR.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.Serialization;
+
+namespace UltimateOrb.Numerics {
+
+    static partial class SR {
+
+        public static string Argument_InvalidNumberStyles { get => @"An undefined NumberStyles value is being used."; }
+
+        public static string Arg_HexStyleNotSupported { get => @"The number style AllowHexSpecifier is not supported on floating point data types."; }
+    }
+}
---
+++ UltimateOrb.Numerics.ComplexF/UltimateOrb/Numerics/ThrowHelper.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+namespace UltimateOrb.Numerics {
+
+    static partial class ThrowHelper {
+
+        [DoesNotReturn]
+        internal static void ThrowNotSupportedException() {
+            throw new NotSupportedException();
+        }
+
+        [DoesNotReturn]
+#pragma warning disable CS8763 // A method marked [DoesNotReturn] should not return.
+        internal static void ThrowOverflowException() {
+            var v = 1u;
+            _ = checked(0u - v);
+        }
+#pragma warning restore CS8763 // A method marked [DoesNotReturn] should not return.
+    }
+}
